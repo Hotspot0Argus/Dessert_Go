4bfcc4ff2cb8ef70de2f6e8fd27e9908ba6a0504
VoChox <791461645@qq.com>
2019-03-19 18:14:46 +0800

    fix 版本代码变化

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url, include
-
+from django.conf.urls import include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    path('$', views.index, name='index'),
 
-    # Test api 
-    url(r'^api/test', views.test_api, name='test_api'),
+    # Test api
+    path('api/test', views.test_api, name='test_api'),
 ]
 
