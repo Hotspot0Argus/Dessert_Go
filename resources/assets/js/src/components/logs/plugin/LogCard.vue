d13fed152c2cda220c3a5725713ca90554d66d8f
rongjx <rjx.163@163.com>
2019-05-16 10:38:11 +0800

    添加了表格

---
+++ resources/assets/js/src/components/logs/plugin/LogCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-card class="box-card">
+      <el-table
+        :data="logInfo.logItems"
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="150">
+        </el-table-column>
+      </el-table>
+      <hr>
+      <span class="is-text-gain is-text-small">{{logInfo.startTime}}</span>
+      <br>
+      <br>
+    </el-card>
+    <br>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['logInfo'],
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
