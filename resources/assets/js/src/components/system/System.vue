e071723aca705140ec158a562091a5a586d49059
bitgavin <473169556@qq.com>
2019-05-16 19:49:59 +0800

    版本管理~

--- resources/assets/js/src/components/system/System.vue
+++ resources/assets/js/src/components/system/System.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="sys">
-    <h1 @click="$router.push({name: 'index'})">System</h1>
-    <el-button type="primary" @click="openDialog">主要按钮</el-button>
+    <div align="middle">
+      <img src="../../assets/dessertgo.jpg" class="image">
+    </div>
+    <p class="tc" > Dessert_Go V1.0 版本</p>
+    <p class="tc"> 北京理工大学假装是软件开发公司出品</p>
+    <br>
+
+    <p class="tc"> 谢谢大家 | https://github.com/Hotspot0Argus/Dessert_Go.git</p>
   </div>
 </template>
 
@@ -20,4 +26,9 @@
 </script>
 
 <style scoped>
+.tc{text-align:center}
+.image{
+  height: 250px;
+  width: 250px
+  }
 </style>
