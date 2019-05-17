85a445bc5e5c7e1ef45917662ec44938394df0a8
VoChox <791461645@qq.com>
2019-05-17 11:03:06 +0800

    confilt

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,15 +1,16 @@
 from django.test import TestCase
-from webapp.models import Menu
+from webapp.models import logs
 
 
 # Create your tests here.
 class MenuTestCase(TestCase):
     def setUp(self):
-        Menu.objects.create(name='123', price=15)
-        Menu.objects.create(name='321', price=51)
+        logs.Log.objects.create(
+            order_id=1,
+            person_id=1,
+            person_name='123')
 
     def test_menu_info(self):
-        item1 = Menu.objects.get(name='123')
-        item2 = Menu.objects.get(name='321')
-        self.assertEqual(item1.get_obj(), {'name': '123', 'price': 15})
-        self.assertEqual(item2.get_obj(), {'name': '321', 'price': 51})
+        self.assertEqual(logs.Log.find_log(1).order_id, 1)
+        self.assertEqual(logs.Log.find_log(1).person_id, 1)
+        self.assertEqual(logs.Log.find_log(1).person_name, '123')
